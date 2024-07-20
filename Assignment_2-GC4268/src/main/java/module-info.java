ca8e2574f2af6efcf8af4e0c46df34d71642c5bd
DipakKumar6-code <126050058+DipakKumar6-code@users.noreply.github.com>
2024-07-19 22:53:06 -0400

    Add files via upload

---
+++ Assignment_2-GC4268/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.assignment_2gc4268 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.assignment_2gc4268 to javafx.fxml;
+    exports com.example.assignment_2gc4268;
+}
